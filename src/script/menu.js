f27ba4687adc354447b2a8e9fc49b1e0d51e0e0c
coolnormal23 <50341105+coolnormal23@users.noreply.github.com>
2022-10-24 13:31:37 -0500

    Basic sidebar

--- src/script/menu.js
+++ src/script/menu.js
@@ -1,37 +1,44 @@
-function showMenu(id, bold)
+function showMenu(id, bold, sidebar)
 {
     if(id != "ArrayAppButtons")
     {
         document.getElementById("ArrayAppButtons").style.display = "none";
+        document.getElementById("ArraySidebarContent").style.display = "none";
         document.getElementById("arraymenu").innerHTML = "Array";
     }
     if(id != "VectorAppButtons")
     {
-        document.getElementById("VectorAppButtons").style.display = "none"; 
+        document.getElementById("VectorAppButtons").style.display = "none";
+        document.getElementById("VectorSidebarContent").style.display = "none";
         document.getElementById("vectormenu").innerHTML = "Vector";
     }
     if(id != "ListAppButtons")
     {
-        document.getElementById("ListAppButtons").style.display = "none"; 
+        document.getElementById("ListAppButtons").style.display = "none";
+        document.getElementById("ListSidebarContent").style.display = "none";
         document.getElementById("listmenu").innerHTML = "Linked List";
     }
     if(id != "StackAppButtons")
     {
-        document.getElementById("StackAppButtons").style.display = "none"; 
+        document.getElementById("StackAppButtons").style.display = "none";
+        document.getElementById("StackSidebarContent").style.display = "none";
         document.getElementById("stackmenu").innerHTML = "Stack";
     }
     if(id != "QueueAppButtons")
     {
-        document.getElementById("QueueAppButtons").style.display = "none"; 
+        document.getElementById("QueueAppButtons").style.display = "none";
+        document.getElementById("QueueSidebarContent").style.display = "none"; 
         document.getElementById("queuemenu").innerHTML = "Queue";
     }
     if(id != "BinaryTreeAppButtons")
     {
-        document.getElementById("BinaryTreeAppButtons").style.display = "none"; 
+        document.getElementById("BinaryTreeAppButtons").style.display = "none";
+        document.getElementById("TreeSidebarContent").style.display = "none";
         document.getElementById("binarytreemenu").innerHTML = "Binary Search Tree";
     }
 
-    document.getElementById(id).style.display = "block"; 
+    document.getElementById(id).style.display = "block";
+    document.getElementById(sidebar).style.display = "block"; 
     
     if(bold == "arraymenu")
     {
@@ -72,12 +79,12 @@ stackmenubutton = document.getElementById("stackmenu");
 queuemenubutton = document.getElementById("queuemenu");
 binarytreemenubutton = document.getElementById("binarytreemenu");
 
-arraymenubutton.onclick = function() {showMenu("ArrayAppButtons", "arraymenu")};
-vectormenubutton.onclick = function() {showMenu("VectorAppButtons", "vectormenu")};
-listmenubutton.onclick = function() {showMenu("ListAppButtons", "listmenu")};
-stackmenubutton.onclick = function() {showMenu("StackAppButtons", "stackmenu")};
-queuemenubutton.onclick = function() {showMenu("QueueAppButtons", "queuemenu")};
-binarytreemenubutton.onclick = function() {showMenu("BinaryTreeAppButtons", "binarytreemenu")};
+arraymenubutton.onclick = function() {showMenu("ArrayAppButtons", "arraymenu", "ArraySidebarContent")};
+vectormenubutton.onclick = function() {showMenu("VectorAppButtons", "vectormenu", "VectorSidebarContent")};
+listmenubutton.onclick = function() {showMenu("ListAppButtons", "listmenu", "ListSidebarContent")};
+stackmenubutton.onclick = function() {showMenu("StackAppButtons", "stackmenu", "StackSidebarContent")};
+queuemenubutton.onclick = function() {showMenu("QueueAppButtons", "queuemenu", "QueueSidebarContent")};
+binarytreemenubutton.onclick = function() {showMenu("BinaryTreeAppButtons", "binarytreemenu", "TreeSidebarContent")};
 
 
-showMenu("ArrayAppButtons", "arraymenu");
+showMenu("ArrayAppButtons", "arraymenu", "ArraySidebarContent");
---
+++ src/script/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("main").style.marginRight = "25%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+  }
